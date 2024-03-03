b34fcc69da099d297a4541b81cd458c36dc4560b
RainLuJ <lujun61@outlook.com>
2024-03-03 09:38:59 +0800

    命令模式开发命令行代码生成器
    使用命令模式开发Picocli命令行代码生成器

---
+++ code-generator-basic/src/main/java/com/rainlu/cli/example/ASCIIArt.java
@@ -0,0 +1,39 @@
+package com.rainlu.cli.example;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+// some exports omitted for the sake of brevity
+
+// 解析命令
+@Command(name = "ASCIIArt", version = "ASCIIArt 1.0", mixinStandardHelpOptions = true) 
+public class ASCIIArt implements Runnable { 
+
+    // 解析选项
+    @Option(names = { "-s", "--font-size" }, description = "Font size") 
+    int fontSize = 19;
+
+    // 解析参数
+    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli", 
+               description = "Words to be translated into ASCII art.")
+    private String[] words = { "Hello,", "picocli" }; 
+
+    @Override
+    public void run() { 
+        // The business logic of the command goes here...
+        // In this case, code for generation of ASCII art graphics
+        // (omitted for the sake of brevity).
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //args = new String[]{"-s", "12", "rainlu", "666666"};
+
+        int exitCode = new CommandLine(new ASCIIArt()).execute(args);
+        System.exit(exitCode);
+    }
+}
